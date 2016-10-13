f3c61eb71180bb6653b5162cde142386cd96d22d
chris-anderson67 <chrisja67@gmail.com>
2016-10-12 20:05:46 -0400

    added new bathroom details activity class

--- README.md
+++
@@ -1,6 +0,0 @@
-#Easy Stream
-##An app to find a place to go.
-![alt text][screenshot]
-
-[screenshot]: rez/welcome_screenshot.png "Screenshot"
-[logo]: rez/logo.png "Logo"
---
+++ app/src/main/java/cs/tufts/edu/easy/Bathroom_Details.java
@@ -0,0 +1,24 @@
+package cs.tufts.edu.easy;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import cs.tufts.edu.easy.R;
+
+public class Bathroom_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bathroom__details);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+
+}
