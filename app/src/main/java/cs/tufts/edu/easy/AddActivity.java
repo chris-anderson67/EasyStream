96d9472e290507507efa77e4a9f597fac4d4fcf3
melaniebelkin <melaniebelkin@gmail.com>
2016-11-12 21:47:35 -0500

    added the add activity and working on post data

---
+++ app/src/main/java/cs/tufts/edu/easy/AddActivity.java
@@ -0,0 +1,71 @@
+package cs.tufts.edu.easy;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import org.apache.http.HttpResponse;
+
+import java.util.concurrent.ExecutionException;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public void onSubmitClick() {
+        final EditText nameText = (EditText) findViewById(R.id.name);
+        String name = nameText.getText().toString();
+        final EditText usernameText = (EditText) findViewById(R.id.username);
+        String username = usernameText.getText().toString();
+        final EditText commentText = (EditText) findViewById(R.id.comment);
+        String comment = commentText.getText().toString();
+
+        final CheckBox maleCheck = (CheckBox) findViewById(R.id.male_check);
+
+        final CheckBox femaleCheck = (CheckBox) findViewById(R.id.female_check);
+        final CheckBox neutralCheck = (CheckBox) findViewById(R.id.neutral_check);
+        String gender = "unknown";
+        if (maleCheck.isChecked() && (femaleCheck.isChecked())) {
+            gender = "neutral";
+        }
+        else if (maleCheck.isChecked()) {
+            gender = "male";
+        }
+        else if (femaleCheck.isChecked()){
+            gender = "female";
+        }
+        else if (neutralCheck.isChecked()) {
+            gender = "neutral";
+        }
+
+        RatingBar cBar = (RatingBar) findViewById(R.id.cleanlinessBar);
+        float cleanlinessF = cBar.getRating();
+        String cleanliness = Float.toString(cleanlinessF);
+
+        RatingBar rBar = (RatingBar) findViewById(R.id.ratingBar);
+        float ratingF = rBar.getRating();
+        String rating = Float.toString(ratingF);
+
+        try {
+            //boolean value determines whether you're requesting all bathrooms data
+            AsyncTask<String, Integer, HttpResponse> task = new PostData().execute(name, username,comment,gender,cleanliness,rating);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ app/src/main/java/cs/tufts/edu/easy/PostData.java
@@ -0,0 +1,53 @@
+package cs.tufts.edu.easy;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.crashlytics.android.Crashlytics;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostData extends AsyncTask<String, Integer, HttpResponse> {
+    @Override
+    // Taken from http://stackoverflow.com/questions/2938502/sending-post-data-in-android
+    protected HttpResponse doInBackground(String... args) {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://45.55.189.20/test_post");
+
+        try {
+            // Set the data
+            EditText title = (EditText)
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair("message[username]", args[0]));
+            nameValuePairs.add(new BasicNameValuePair("message[content]", args[1]));
+            nameValuePairs.add(new BasicNameValuePair("message[app_id]", "4"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            return response;
+        }
+        catch (ClientProtocolException e) {
+            Crashlytics.logException(e);
+            Log.e("**** postMessage(): ", e.toString());
+            return null;
+        }
+        catch (IOException e) {
+            Crashlytics.logException(e);
+            Log.e("**** postMessage(): ", e.toString());
+            return null;
+        }
+    }
+
+}
