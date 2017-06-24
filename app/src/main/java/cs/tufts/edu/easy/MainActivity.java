5c64ec4656b1d1d494a8bb7920d6cd43b7b55887
chris-anderson67 <chrisja67@gmail.com>
2017-06-24 14:32:21 -0400

    Removed app-indexing code, updated gitignore

--- app/src/main/java/cs/tufts/edu/easy/MainActivity.java
+++ app/src/main/java/cs/tufts/edu/easy/MainActivity.java
@@ -5,26 +5,16 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import com.crashlytics.android.Crashlytics;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
     private int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 0;
     private MainActivity context;
     private LocationManager mLocationManager;
@@ -36,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
         this.mLocationManager = (LocationManager) this.context.getSystemService(this.context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -64,35 +53,5 @@ public class MainActivity extends AppCompatActivity {
     public void onClickRateButton(View view) {
         Intent launchRateIntent = new Intent(MainActivity.this, AddActivity.class);
         MainActivity.this.startActivity(launchRateIntent);
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
--- app/src/main/java/cs/tufts/edu/easy/MapsActivity.java
+++ app/src/main/java/cs/tufts/edu/easy/MapsActivity.java
@@ -1,17 +1,9 @@
 package cs.tufts.edu.easy;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
@@ -23,20 +15,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
     public static HashMap<Integer, Bathroom> bathroomMap;
-    private GoogleApiClient client;
-    int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,17 +34,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         bathroomMap = new HashMap<>();
-
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-
 
     @Override
     public void onInfoWindowClick(Marker marker) {
-        Log.v("INFO WINDOWCLICKED", "CLICKED INFO WINDOW!!");
         Intent myIntent = new Intent(this, Bathroom_Details.class);
         myIntent.putExtra("bathroom_id", (Integer) marker.getTag());
-        Log.d("MARKER_TAG: ", Integer.toString((Integer) marker.getTag()));
         startActivity(myIntent);
     }
 
@@ -69,9 +51,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng sydney = new LatLng(42.408250, -71.120336);
 
         try {
-            //boolean value determines whether you're requesting all bathrooms data
+            /* boolean value determines whether you're requesting all bathrooms data */
             s = new GetData(true).execute().get();
-            Log.v("JSONSTRING", s);
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
@@ -86,23 +67,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             try {
                 JSONObject oneObject = jArray.getJSONObject(i);
                 Bathroom currBathroom = new Bathroom(oneObject);
-                Log.v("CURBATHROOM:", currBathroom.bathroom_name);
-                Log.v("ID:", Integer.toString(currBathroom.id));
                 bathroomMap.put(currBathroom.id, currBathroom);
-                Log.v("BATHROOM_INSERTED", currBathroom.bathroom_name);
                 double lat = currBathroom.latitude;
                 double lon = currBathroom.longitude;
-                Log.v("BATHROOM_MAP", String.valueOf(bathroomMap));
-
                 LatLng bathroom = new LatLng(lat, lon);
                 Marker marker = mMap.addMarker(new MarkerOptions()
                         .position(bathroom)
                         .title(currBathroom.bathroom_name)
                         .snippet(currBathroom.address));
                 marker.setTag(currBathroom.id);
-
-                Log.d("***JOBJECT_lat***", Double.toString(lat));
-                Log.d("***JOBJECT_lon***", Double.toString(lon));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -110,35 +83,5 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16));
         mMap.setOnInfoWindowClickListener(this);
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Maps Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
