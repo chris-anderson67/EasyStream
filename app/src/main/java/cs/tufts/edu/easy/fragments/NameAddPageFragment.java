637df3b2c16fec6ca1ee994c4e4179102f77ab17
chris-anderson67 <chrisja67@gmail.com>
2017-07-31 01:22:30 -0400

    cleaned up name fragment

--- app/src/main/java/cs/tufts/edu/easy/fragments/NameAddPageFragment.java
+++ app/src/main/java/cs/tufts/edu/easy/fragments/NameAddPageFragment.java
@@ -1,8 +1,5 @@
 package cs.tufts.edu.easy.fragments;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,31 +10,10 @@ import cs.tufts.edu.easy.models.Bathroom;
 public class NameAddPageFragment extends BaseAddPageFragment {
 
     private EditText nameField;
-    private String name;
 
     @Override
     protected void setupViews(View rootView) {
         nameField = (EditText) rootView.findViewById(R.id.name_add_fragment_edit_text);
-        addTextListener();
-    }
-
-    private void addTextListener() {
-        nameField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.e("TAG", s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                name = s.toString();
-            }
-        });
     }
 
     @Override
@@ -47,7 +23,7 @@ public class NameAddPageFragment extends BaseAddPageFragment {
 
     @Override
     public Bathroom getUpdatedBathroom(Bathroom initialBathroom) {
-        initialBathroom.name = name;
+        initialBathroom.name = nameField.getText().toString();
         return initialBathroom;
     }
 }
